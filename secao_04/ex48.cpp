a4f24c89bca36c768dfe3e1ff60e5d3aefbc70fb
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2019-11-12 16:07:15 -0300

    ex48.cpp

---
+++ secao_04/ex48.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int secs, horas, minutos, segundos;
+
+    cout << "Digite o valor em segundos: ";
+    cin >> secs;
+
+    if ( secs % 3600 == 0) {
+        cout << "Horas: " << secs / 3600;
+    }
+
+    cout << "Horas: " << secs / 3600 << endl;
+
+    cout << "Minutos: " << (secs % 3600) / 60 << endl;
+
+    cout << "Segundos: " << secs % 3600 % 60 << endl;
+    
+    return 0;
+}
