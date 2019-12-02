6cb76412461c8fbc78c9b27be29a6d211327d362
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2019-12-02 16:25:05 -0300

    sec6 ex17

---
+++ secao_06/ex13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    int num;
+
+    cout << "Número: ";
+    cin >> num;
+
+    for (int i = 0; i <= num; i++)
+        if( i % 2 == 0)
+            cout << i << endl;
+
+    return 0;
+}
---
+++ secao_06/ex14.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    int num;
+
+    cout << "Número: ";
+    cin >> num;
+
+    for (int i = num; i >= 0; i--){
+        if(i % 2 == 0)
+            cout << i << endl;
+    }
+
+    return 0;
+}
